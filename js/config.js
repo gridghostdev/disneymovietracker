45916f7783f4a4fee6791af87ca2450c0f28df53
gridghost.dev <cain@gridghost.dev>
2025-05-17 15:09:20 -0400

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
     tmdb: {
-        // Credentials moved to credentials.js (gitignored)
-        apiKey: typeof CREDENTIALS !== 'undefined' ? CREDENTIALS.tmdb.apiKey : '',
-        accessToken: typeof CREDENTIALS !== 'undefined' ? CREDENTIALS.tmdb.accessToken : '',
+        apiKey: '2026c676b8fb58f7b3352d1731fce4e7',
+        accessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDI2YzY3NmI4ZmI1OGY3YjMzNTJkMTczMWZjZTRlNyIsIm5iZiI6MTc0NzI1OTM5My42NjYsInN1YiI6IjY4MjUxMDAxZTViMzRkMzYwNjdlODg5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NeXbmuWsan-qGIrPx0DeS1yO0DgiUvzVxcUQrKS04Rw',
         baseUrl: 'https://api.themoviedb.org/3',
         imageBaseUrl: 'https://image.tmdb.org/t/p/'
     },
