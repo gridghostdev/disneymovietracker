7225d592020eecb04e19de8bf6ab390d736d8506
gridghost.dev <cain@gridghost.dev>
2025-05-17 15:05:55 -0400

    Add files via upload

---
+++ js/credentials.sample.js
@@ -0,0 +1,7 @@
+// This is a sample file. Copy to credentials.js and fill in your API keys
+const CREDENTIALS = {
+    tmdb: {
+        apiKey: 'YOUR_TMDB_API_KEY_HERE',
+        accessToken: 'YOUR_TMDB_ACCESS_TOKEN_HERE'
+    }
+};
