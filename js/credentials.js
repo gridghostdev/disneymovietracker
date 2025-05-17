d302b88f3f80f93228bc7e5aede8343a3ea05171
gridghost.dev <cain@gridghost.dev>
2025-05-17 15:14:28 -0400

    Delete js directory

--- js/credentials.js
+++
@@ -1,7 +0,0 @@
-// This file contains API credentials and should be added to .gitignore
-const CREDENTIALS = {
-    tmdb: {
-        apiKey: '2026c676b8fb58f7b3352d1731fce4e7',
-        accessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDI2YzY3NmI4ZmI1OGY3YjMzNTJkMTczMWZjZTRlNyIsIm5iZiI6MTc0NzI1OTM5My42NjYsInN1YiI6IjY4MjUxMDAxZTViMzRkMzYwNjdlODg5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NeXbmuWsan-qGIrPx0DeS1yO0DgiUvzVxcUQrKS04Rw'
-    }
-};
--- js/credentials.sample.js
+++
@@ -1,7 +0,0 @@
-// This is a sample file. Copy to credentials.js and fill in your API keys
-const CREDENTIALS = {
-    tmdb: {
-        apiKey: 'YOUR_TMDB_API_KEY_HERE',
-        accessToken: 'YOUR_TMDB_ACCESS_TOKEN_HERE'
-    }
-};
